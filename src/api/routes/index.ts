efbca34b69e218af34ed52e7794b714153eb3600
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-10-27 18:56:52 -0300

    feat: routes

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,19 +1,30 @@
 import { Router } from "express";
 import { usersRoutes } from './users.routes'
+import { productsRoutes } from './products.routes'
+import { categoriesRoutes } from './categories.routes'
+import { ingredientsRoutes } from './ingredients.routes'
+import { imagesRoutes } from './images.routes'
+import { favoritesRoutes } from './favorites.routes'
+
+/*
+import { ordersRoutes } from './orders.routes'
+import { paymentsRoutes } from './payments.routes'
+*/
+
 
 
 const routes = Router()
 
 routes.use('/session', usersRoutes)
 routes.use('/users', usersRoutes)
-routes.use('/products', usersRoutes)
+routes.use('/products', productsRoutes)
 
-/*
+
 routes.use('/categories', categoriesRoutes)
 routes.use('/ingredients', ingredientsRoutes)
 routes.use('/images', imagesRoutes)
 routes.use('/favorites', favoritesRoutes)
-*/
+
 
 
 export {
