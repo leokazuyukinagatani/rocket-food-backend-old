5a3d6d708e52f8c962c1c71cfc04a43e4c91730a
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-10-27 14:14:47 -0300

    fix: app

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import "express-async-errors";
-import { AppError } from "./utils/AppError";
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import { routes } from "./routes";
 import swaggerUi from 'swagger-ui-express'
@@ -14,26 +13,5 @@ app.use(express.json());
 app.use(routes);
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
-app.use(
-  (
-    error: AppError,
-    request: Request,
-    response: Response,
-    next: NextFunction
-  ) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-        status: "error",
-        message: error.message,
-      });
-    }
-    console.log(error);
-
-    return response.status(500).json({
-      status: "error",
-      message: "Internal server error",
-    });
-  }
-);
 
 export { app };
