025510b43fd5cf5b59f96bff616d258270073eee
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-11-08 08:47:45 -0300

    feat: authenticated

--- src/api/routes/users.routes.ts
+++ src/api/routes/users.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { UsersController } from '../controllers/UsersController'
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const usersRoutes = Router()
 const usersController = new UsersController()
 
 
+usersRoutes.use(ensureAuthenticated)
 usersRoutes.post('/', usersController.create)
-
+usersRoutes.get('/', usersController.show)
 
 export {
   usersRoutes
