e69bc3d35270685590d20d1582d137f0d5db5468
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-12-07 20:29:41 -0300

    fix: findImage

--- src/api/controllers/UserAvatarController.ts
+++ src/api/controllers/UserAvatarController.ts
@@ -3,7 +3,7 @@ import { DiskStorage } from "../../providers/DiskStorage";
 import { UserRepository } from "../repositories/users/UserRepository";
 import { UserShowService } from "../services/users/UserShowService";
 import { UserUpdateService } from "../services/users/UserUpdateService";
-import { prisma } from '../database/prisma'
+import { prisma } from "../database/prisma";
 import { AppError } from "../utils/AppError";
 
 export class UserAvatarController {
@@ -15,30 +15,28 @@ export class UserAvatarController {
     const userShowService = new UserShowService(userRepository);
     const userUpdateService = new UserUpdateService(userRepository);
 
-
     const diskStorage = new DiskStorage();
 
-    const user =  await userShowService.execute(user_id)
+    const user = await userShowService.execute(user_id);
 
-    if(!user) {
-      throw new AppError('Only authenticated users can change avatar', 401);
-    } 
-    if(!avatarFilename) {
-      throw new AppError('Filename is required', 404);
+    if (!user) {
+      throw new AppError("Only authenticated users can change avatar", 401);
+    }
+    if (!avatarFilename) {
+      throw new AppError("Filename is required", 404);
     }
 
-    const imageResult = await prisma.image.findFirst(user_id)
+    const imageResult = await prisma.image.findFirst({
+      where: { User: user_id },
+    });
 
-
-    
-    if(imageResult) {
+    if (imageResult) {
       await diskStorage.deleteFile(imageResult.filename);
     }
-  
-    const image = await diskStorage.saveFile(avatarFilename,'users')
-    
 
-    const userResult = await userUpdateService.execute({ id: user.id , image });
+    const image = await diskStorage.saveFile(avatarFilename, "users");
+
+    const userResult = await userUpdateService.execute({ id: user.id, image });
 
     if (userResult instanceof AppError) {
       return response.status(400).json(userResult.message);
