20945f2195c33843ff3115085eb11970833a2f20
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-12-07 20:14:56 -0300

    feat: route avatar upload

--- src/api/routes/users.routes.ts
+++ src/api/routes/users.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
+import multer from "multer";
+import { UserAvatarController } from "../controllers/UserAvatarController";
 import { UsersController } from "../controllers/UsersController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { MULTER } from "../configs/upload";
 
 const usersRoutes = Router();
 const usersController = new UsersController();
+const upload = multer(MULTER);
+const userAvatarController = new UserAvatarController(); 
 
 usersRoutes.post("/", usersController.create);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update);
 usersRoutes.get("/", ensureAuthenticated, usersController.show);
 usersRoutes.post("/acl", ensureAuthenticated, usersController.access);
 
