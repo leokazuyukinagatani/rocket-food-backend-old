70687f590bc10f9fc2d26ff28f85b4eda3c28202
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-12-07 13:12:14 -0300

    feat: ingredient repository

--- src/api/repositories/ingredients/IngredientRepository.ts
+++ src/api/repositories/ingredients/IngredientRepository.ts
@@ -1,76 +1,65 @@
-import { prisma } from '../../database/prisma'
+import { prisma } from "../../database/prisma";
 
-export interface IIngredient{
-  id?:string
-  name:string
-  description:string
-  imageId:string
+export interface IIngredient {
+  id?: string;
+  name: string;
+  description: string;
 }
 export class IngredientRepository {
-
   async index() {
-    const ingredients = await prisma.ingredient.findMany()
-    
-    return ingredients
+    const ingredients = await prisma.ingredient.findMany();
+
+    return ingredients;
   }
 
-  async findById(id:string) {
+  async showById(id: string) {
     const ingredient = await prisma.ingredient.findFirst({
       where: {
-        id
-      }
-    })
-    return ingredient
+        id,
+      },
+    });
+    return ingredient;
   }
 
-  async findByName(name:string) {
+  async showByName(name: string) {
     const ingredient = await prisma.ingredient.findFirst({
       where: {
-        name
-      }
-    })
-
-    return ingredient
-  }
-
-
-  async create({ name, description, imageId }:IIngredient) {
-    const newIngredient = await prisma.ingredient.create({
-      data: {
         name,
-        description,
-        imageId
-      }
-    })
+      },
+    });
 
-    return { id: newIngredient.id }
+    return ingredient;
   }
 
-  async update({ id, name, description }:IIngredient) {
-    
+  async create({ name, description }: IIngredient) {
+    const newIngredient = await prisma.ingredient.create({
+      data: { name, description },
+    });
+
+    return { id: newIngredient.id };
+  }
+
+  async update({ id, name, description }: IIngredient) {
     const ingredient = await prisma.ingredient.update({
       where: {
-        id
+        id,
       },
       data: {
         name,
-        description
-      }
-    })
+        description,
+      },
+    });
 
-    return ingredient
+    return ingredient;
   }
 
-
-  async delete(id:string) {
+  async delete(id: string) {
     const deletedIngredient = await prisma.ingredient.delete({
       where: {
-        id
-      }
-    })
+        id,
+      },
+    });
 
-    return { id: deletedIngredient.id }
+    return { id: deletedIngredient.id };
   }
-
 }
-
