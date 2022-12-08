bbc33f4891ac93375b0a15aa6a5dc428a715a9ca
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-12-08 18:34:36 -0300

    feat: upload image to cloudinary and save in db

--- src/api/controllers/UserAvatarController.ts
+++ src/api/controllers/UserAvatarController.ts
@@ -5,52 +5,70 @@ import { UserShowService } from "../services/users/UserShowService";
 import { AppError } from "../utils/AppError";
 import { ImageRepository } from "../repositories/images/ImageRepository";
 import { ImageCreateService } from "../services/images/ImageCreateService";
-import { ImageShowService } from "../services/images/ImageShowService";
 import { UserUpdateAvatarService } from "../services/users/UserUpdateAvatarService";
+import { CustomRequest } from "../middlewares/ensureAuthenticated";
+import { ImageShowByNameService } from "../services/images/ImageShowByNameService";
+import { ImageUpdateService } from "../services/images/ImageUpdateService";
+import { ImageUpsertService } from "../services/images/ImageUpsertService";
 
 export class UserAvatarController {
-  async update(request: Request, response: Response) {
-    const user_id = request.body.user.id;
-    console.log(user_id);
-    if (!request.file) {
-      throw new AppError("File is required", 404);
+  async update(request: CustomRequest, response: Response) {
+    const user_id = request.user?.id;
+
+    const userRepository = new UserRepository();
+    const userShowService = new UserShowService(userRepository);
+    const userUpdateAvatarService = new UserUpdateAvatarService(userRepository);
+    const imageRepository = new ImageRepository();
+    const imageCreateService = new ImageCreateService(imageRepository);
+    const imageUpdateService = new ImageUpdateService(imageRepository);
+    const imageShowByNameService = new ImageShowByNameService(imageRepository);
+
+    const imageUpsertService = new ImageUpsertService(imageRepository);
+    const diskStorage = new DiskStorage();
+    if (typeof user_id !== "string") {
+      throw new AppError("not user_id found");
     }
-    const avatarFilename = request.file.filename;
-    console.log('AvatarFileName ===>',avatarFilename);
-    // const userRepository = new UserRepository();
-    // const userShowService = new UserShowService(userRepository);
-    // const userUpdateAvatarService = new UserUpdateAvatarService(userRepository);
-    // const imageRepository = new ImageRepository();
-    // const imageCreateService = new ImageCreateService(imageRepository);
-    // const imageShowService = new ImageShowService(imageRepository);
+    const user = await userShowService.execute(user_id);
+    console.log("User ====>", user);
+    if (!user) {
+      throw new AppError("Only authenticated users can change avatar", 401);
+    }
 
-    // const diskStorage = new DiskStorage();
+    const imageResult = await imageShowByNameService.execute(
+      `avatar-${user_id}`
+    );
+    console.log("imageResult =", imageResult);
+    if (imageResult) {
+      await diskStorage.deleteFile(imageResult.filename);
+    }
 
-    // const user = await userShowService.execute(user_id);
-    // console.log('User ====>', user)
-    // if (!user) {
-    //   throw new AppError("Only authenticated users can change avatar", 401);
+    const image = await diskStorage.saveFile(`avatar-${user_id}`, "users");
+    if (!image) {
+      throw new AppError("Could not save");
+    }
+    console.log("image ===>", image);
+    
+    // if(imageResult) {
+    //   await imageUpdateService.execute({
+    //     id: imageResult.id,
+    //     filename: image.filename,
+    //     url: image.url})
+    // }else {
+    //   await imageCreateService.execute(image)
     // }
 
-    // const imageResult = await imageShowService.execute(avatarFilename);
+    const imageAvatar = await imageUpsertService.execute(image);
+    
 
-    // if (imageResult) {
-    //   await diskStorage.deleteFile(imageResult.filename);
-    // }
+    const userResult = await userUpdateAvatarService.execute({
+      id: user_id,
+      image: imageAvatar,
+    });
 
-    // const image = await diskStorage.saveFile('arquivo-teste', "users");
-    // await imageCreateService.execute(image);
+    if (userResult instanceof AppError) {
+      return response.status(400).json(userResult.message);
+    }
 
-    // if (!image) {
-    //   throw new AppError("Could not save");
-    // }
-
-    // const userResult = await userUpdateAvatarService.execute({ id: user.id, image });
-
-    // if (userResult instanceof AppError) {
-    //   return response.status(400).json(userResult.message);
-    // }
-
-    // return response.status(201).json(userResult);
+    return response.status(201).json(userResult);
   }
 }
