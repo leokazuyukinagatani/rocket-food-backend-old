3930137c8b6f0f98e6146244fd94d43a72ea360d
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-12-08 18:33:30 -0300

    feat: rename image

--- src/api/configs/upload.ts
+++ src/api/configs/upload.ts
@@ -1,19 +1,18 @@
 import path from "path";
 import multer from "multer";
 import crypto from "crypto";
+import { CustomRequest } from "../middlewares/ensureAuthenticated";
 
-export const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
+export const TMP_FOLDER = path.resolve(__dirname, "..", "..", "..", "tmp");
 export const UPLOAD_FOLDER = path.resolve(TMP_FOLDER, "uploads");
-
 export const MULTER = {
   storage: multer.diskStorage({
     destination: TMP_FOLDER,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString("hex");
-      const filename = `${fileHash}-${file.originalname}`;
-
-      return callback(null, filename);
+    filename(request: CustomRequest, file, callback) {
+      // const fileHash = crypto.randomBytes(10).toString("hex");
+ 
+      const filename = request.user?.id ? `avatar-${request.user.id}` : "avatar-teste";
+      if (typeof filename === "string") return callback(null, filename);
     },
   }),
 };
-
